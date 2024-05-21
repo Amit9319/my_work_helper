70b8b48efbecd6f2281124dc0be1ce73106611a7
Amit9319 <sharmaamit9319@gmail.com>
2024-05-22 01:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def download_image():
     return send_file('table.png', mimetype='image/png', as_attachment=True, attachment_filename='table.png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
